7d6d8b9ba073a14e2768b8d4681f227d649e8cb8
dpwr1 <dpwr1@DESKTOP-RNV853T>
2022-03-15 20:28:05 -0500

    Add project files.

---
+++ MapTool1.cs
@@ -0,0 +1,62 @@
+﻿using ArcGIS.Core.CIM;
+using ArcGIS.Core.Data;
+using ArcGIS.Core.Geometry;
+using ArcGIS.Desktop.Catalog;
+using ArcGIS.Desktop.Core;
+using ArcGIS.Desktop.Editing;
+using ArcGIS.Desktop.Extensions;
+using ArcGIS.Desktop.Framework;
+using ArcGIS.Desktop.Framework.Contracts;
+using ArcGIS.Desktop.Framework.Dialogs;
+using ArcGIS.Desktop.Framework.Threading.Tasks;
+using ArcGIS.Desktop.Layouts;
+using ArcGIS.Desktop.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MntopoSpotElevation
+{
+    internal class MapTool1 : MapTool
+    {
+        public MapTool1()
+        {
+            IsSketchTool = true;
+            SketchType = SketchGeometryType.Point;
+            SketchOutputMode = SketchOutputMode.Map;
+        }
+
+        protected override Task<bool> OnSketchCompleteAsync(Geometry geometry)
+        {
+            var coord = GeometryEngine.Instance.Project(geometry, SpatialReferences.WGS84) as MapPoint;
+            if (coord != null)
+            {
+                double lon = coord.X;
+                double lat = coord.Y;
+
+                // query MnTOPO
+                double elevationFeet = MntopoResponse.GetElevationAtLonLatFeet(lon, lat);
+                if (elevationFeet < 0) // GetElevationAtLonLatFeet will return -1 if the server retruns an error.
+                {
+                    string msg = "The location selected is outside of the data extents \nPlease try again.";
+                    ArcGIS.Desktop.Framework.Dialogs.MessageBox.Show(msg);
+                    return Task.FromResult(false);
+                }
+                else  // server returned a result
+                {
+                    string msg = $"Elevation: {elevationFeet} feet";
+                    ArcGIS.Desktop.Framework.Dialogs.MessageBox.Show(msg);
+                    return Task.FromResult(true);
+                }
+            }
+            else
+            {
+                String msg = "Selected coordinate is empty.";
+                ArcGIS.Desktop.Framework.Dialogs.MessageBox.Show(msg);
+                return Task.FromResult(false);
+            }
+        }
+    }
+}
---
+++ MntopoResponse.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace MntopoSpotElevation
+{
+    static class MntopoResponse
+    {
+        private static double GetElevationAtLonLat(double lon, double lat)
+        {
+            string url = $"http://arcgis.dnr.state.mn.us/mndnr/rest/services/elevation/DEM1m/MapServer/exts/ElevationsSOE_NET/ElevationLayers/0/GetElevationAtLonLat?lon={lon}&lat={lat}&f=pjson";
+            WebRequest request = WebRequest.Create(url);
+            WebResponse response = request.GetResponse();
+            using (Stream dataStream = response.GetResponseStream())
+            {
+                StreamReader reader = new StreamReader(dataStream);
+                string jsonString = reader.ReadToEnd();
+
+                response.Close();
+
+                // Regex is used here because there is version conflicts with the .dll refereneces used in System.Text.Json
+                string pattern = @"\d*[.]\d*";
+                string elevationString = Regex.Match(jsonString, pattern).Value;
+                
+                // cast to double
+                try
+                {
+                    double elevation = Convert.ToDouble(elevationString);
+                    return elevation;
+                }
+
+                // If the user clicks out of data extents, the server will return json containing an error code.
+                // The casting will throw an error as there will not be a regex match.
+                catch
+                {
+                    return -1;
+                }
+                
+            }
+        }
+
+        public static double GetElevationAtLonLatMeters(double lon, double lat)
+        {
+            return Math.Round(GetElevationAtLonLat(lon, lat), 2);
+        }
+
+        public static double GetElevationAtLonLatFeet(double lon, double lat)
+        {
+            return Math.Round((GetElevationAtLonLat(lon, lat) * 3.28084), 1);
+        }
+    }
+}
